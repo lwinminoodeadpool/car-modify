6547e89dfb73ffd59b4fd3c241c4920c0fd18e80
lwinminoodeadpool <55123218+lwinminoodeadpool@users.noreply.github.com>
2020-01-28 14:29:36 +0630

    Update index.js

--- index.js
+++ index.js
@@ -592,21 +592,6 @@ app.post('/webhook', (req, res) => {
           console.log(error)
         })
       }
-
-      
-
-       
-
-        requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, 
-          genericMessage
-        ).then( response => {
-          console.log(response)
-        }).fail( error => {
-          console.log(error)
-        })
-      }
-
-       //if user alloy wheel for rent 
     
        
      
