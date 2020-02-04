bf894ad11fe4ca03ee8a269be868c76d2baed66b
lwinminoodeadpool <55123218+lwinminoodeadpool@users.noreply.github.com>
2020-02-04 13:12:32 +0630

    Update index.js

--- index.js
+++ index.js
@@ -802,8 +802,17 @@ app.post('/webhook', (req, res) => {
             }
           }
         }
-      }
+        
+       
 
+      }
+      requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, 
+          genericMessage
+        ).then( response => {
+          console.log(response)
+        }).fail( error => {
+          console.log(error)
+        })
                     
                   
       });
